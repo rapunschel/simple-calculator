b36153eb5f483ee4d8f078c134b40054571e3294
rapunschel <rapunschel.gaming@gmail.com>
2025-07-06 01:00:00 +0200

    More styling, added wrapper for btn rows

--- calculator.js
+++ calculator.js
@@ -1,23 +1,30 @@
 const calculator = document.querySelector(".calculator");
 function initLayout() {
-    let layout = [ ["AC", "DEL"],
+    let layout = [ ["", "", "AC", "DEL"],
                     [7,8,9, '/'],
                     [4,5,6,'*'], 
                     [1,2,3, '-'],
                     ['.',0, '=', '+']];
     const operators = "/+-*=".split('');
     const undoOperators = "AC DEL".split(' ');
-
+    let keypad = document.createElement("div"); // Wrapper for btn-rows
+    keypad.className = "calc-keypad"
     layout.forEach((row) => {
+
         let buttons = document.createElement("div");
         buttons.className = "calc-row";
-        
+        keypad.appendChild(buttons);
+
         row.forEach((btn) => {
             let button = document.createElement("button");
             button.className = "calc-btn"
             button.textContent = btn;
             buttons.appendChild(button);
-
+            
+            if (btn === "") {
+                button.style.visibility = "hidden";
+                return
+            }
             if (undoOperators.includes(btn)) {
                 button.classList.add("undo-btn");
                 return;
@@ -28,8 +35,9 @@ function initLayout() {
             }
             button.classList.add("num-btn");
         })
-        calculator.appendChild(buttons);
     })
+    calculator.appendChild(keypad);
+
 }
 
 console.log(calculator);
